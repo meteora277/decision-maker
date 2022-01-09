60108c7e7c2eeecb6b2b82d1b0aa9b08106ac394
Bita <bita.janzadeh@hotmail.com>
2022-01-08 19:21:14 -0500

    Set up mailgun API in mailgun.js file to send test emails

--- mailgun.js
+++ mailgun.js
@@ -1,6 +1,21 @@
-const request = require('request');
+// load .env data into process.env
+require("dotenv").config();
 
 /**
  * Makes a single API request to send an email to user.
- */
+*/
 
+ var api_key = process.env.API_KEY;
+ var domain = 'sandbox491a8df337a74dd78b4d40467c043b8f.mailgun.org';
+ var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+ var data = {
+   from: 'Excited User <me@samples.mailgun.org>',
+   to: 'bita.janzadeh@hotmail.com',
+   subject: 'Please work',
+   text: 'Hey!! I hid my API key, does this work?'
+ };
+
+ mailgun.messages().send(data, function (error, body) {
+   console.log(body);
+ });
