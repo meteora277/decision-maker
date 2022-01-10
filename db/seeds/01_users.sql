fb90e549d95281b58185bf3e8cf551383c9984e0
meteora277 <justin.s.diaz@gmail.com>
2022-01-10 16:21:34 -0500

    create more test polls

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -128,6 +128,13 @@ VALUES (5, 4, 2);
 
 
 
+
+
+
+
+
+
+
 INSERT INTO
   polls (
     email_address,
@@ -136,14 +143,14 @@ INSERT INTO
     admin_link,
     poll_link,
     is_active
-  );
+  )
 VALUES
   (
     'justin.s.diaz@gmail.com',
     'Should you watch Arcane on Netflix',
     FALSE,
     'mm3uwu',
-    'j57cxd',
+    'ouo789',
     TRUE
   );
 --
@@ -152,153 +159,201 @@ INSERT INTO choices (poll_id, title, description)
 VALUES (2, 'Yes', 'cute cafe w/ alot of plants on Queen St. West, has a cozy garden patio in the warmer months.');
 
 INSERT INTO choices (poll_id, title, description)
-VALUES (3, 'No', 'Modern coffee shop that focuses on bringing interesting coffees to market, you''l find alot of fermented flavor notes here.');
+VALUES (2, 'No', 'Modern coffee shop that focuses on bringing interesting coffees to market, you''l find alot of fermented flavor notes here.');
+
+INSERT INTO names (name)
+VALUES ('Mel');
+
+INSERT INTO names (name)
+VALUES ('TJ');
+
+INSERT INTO names (name)
+VALUES ('Wiggles');
+
+INSERT INTO names (name)
+VALUES ('Octavia');
+
+INSERT INTO names (name)
+VALUES ('Frieda');
+
+
+-- one vote insert per option in poll
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (6, 2, 6);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (7, 1, 6);
+
+--
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (6, 1, 7);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (7, 2, 7);
 
 
 
--- INSERT INTO names (name)
--- VALUES ('Mel');
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (6, 1, 8);
 
--- INSERT INTO names (name)
--- VALUES ('TJ');
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (7, 2, 8);
 
--- INSERT INTO names (name)
--- VALUES ('Wiggles');
+--
 
--- INSERT INTO names (name)
--- VALUES ('Octavia');
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (6, 1, 9);
 
--- INSERT INTO names (name)
--- VALUES ('Frieda');
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (7, 2, 9);
 
+--
 
--- -- one vote insert per option in poll
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 2, 1);
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (6, 1, 10);
 
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 1, 1);
-
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 2);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 2);
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 3);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 3);
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 4);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 4);
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 5);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 5);
-
--- --
-
-
--- INSERT INTO names (name)
--- VALUES ('Mel');
-
--- INSERT INTO names (name)
--- VALUES ('TJ');
-
--- INSERT INTO names (name)
--- VALUES ('Wiggles');
-
--- INSERT INTO names (name)
--- VALUES ('Octavia');
-
--- INSERT INTO names (name)
--- VALUES ('Frieda');
-
-
--- -- one vote insert per option in poll
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 2, 1);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 1, 1);
-
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 2);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 2);
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES(1, 1, 3);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 3);
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 4);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 4);
-
--- --
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (1, 1, 5);
-
--- INSERT INTO votes (choice_id, vote_weight, name_id)
--- VALUES (2, 2, 5);
-
--- --
-
--- INSERT INTO choices (poll_id, title, description)
--- VALUES (2, 'Yes', 'cute cafe w/ alot of plants on Queen St. West, has a cozy garden patio in the warmer months.');
-
--- INSERT INTO choices (poll_id, title, description)
--- VALUES (3, 'No', 'Modern coffee shop that focuses on bringing interesting coffees to market, you''l find alot of fermented flavor notes here.');
-
-
--- INSERT INTO
---   polls (
---     email_address,
---     question,
---     anonymous,
---     admin_link,
---     poll_link,
---     is_active
---   );
--- VALUES
---   (
---     'justin.s.diaz@gmail.com',
---     'Should you watch Arcane on Netflix',
---     FALSE,
---     'mm3uwu',
---     'j57cxd',
---     TRUE
---   );
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (7, 2, 10);
 
 
 
+
+
+
+
+INSERT INTO
+  polls (
+    email_address,
+    question,
+    admin_link,
+    poll_link,
+    is_active,
+    anonymous
+  )
+VALUES
+  (
+    'justin.s.diaz@gmail.com',
+    'What should I do to make my new foster cat love me?',
+    'nya345',
+    'nyanko',
+    TRUE,
+    TRUE
+  );
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Give Her Space', 'Give her time to adapt to the new environment.');
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Constantly bother her so she gets used to it', 'I''m going to constantly bother her anyways, so may as well start early.');
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Give her food', 'Appeal to her stomach.');
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Scream randomly throughout the day', 'Nothing will surprise her after a while.');
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Starring contest', 'Assert your authority, loser looks away first.');
+
+INSERT INTO names (name)
+VALUES ('Justin');
+
+INSERT INTO names (name)
+VALUES('Saranya');
+
+INSERT INTO names (name)
+VALUES ('Bita');
+
+INSERT INTO names (name)
+VALUES ('Uwu');
+
+INSERT INTO names (name)
+VALUES ('John');
+
+--
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (8, 5, 11);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (9, 4, 11);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (10, 3, 11);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (11, 2, 11);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (12, 1, 11);
+
+--
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (8, 5, 12);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (9, 3, 12);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (10, 4, 12);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (11, 2, 12);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (12, 1, 12);
+
+--
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (8, 4, 13);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES(9, 2, 13);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (10, 1, 13);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (11, 5, 13);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (12, 3, 13);
+
+--
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (8, 5, 14);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (9, 4, 14);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (10, 3, 14);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (11, 2, 14);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (12, 1, 14);
+
+--
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (8, 1, 15);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (9, 5, 15);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (10, 3, 15);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES(11, 2, 15);
+
+INSERT INTO votes (choice_id, vote_weight, name_id)
+VALUES (12, 4, 15);
 
