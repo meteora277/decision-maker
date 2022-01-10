131c51436b98eddd951b8d460a0cb6d222caf95f
Bita <bita.janzadeh@hotmail.com>
2022-01-09 20:57:08 -0500

    Creating rankedOrder function, saves choice ranking in an array

--- public/scripts/voting.js
+++ public/scripts/voting.js
@@ -1,5 +1,5 @@
 const draggable_list = document.getElementById('draggable-list');
-const check = document.getElementById('submit-poll');
+const check = document.getElementById('submit-poll-btn');
 
 console.log("hi");
 
@@ -8,14 +8,16 @@ const myChoices = [
   'apple',
   'orange',
   'banana',
-  'peach'
+  'peach',
+  'watermelon',
+  'pomegrante',
+  'strawberry'
 ];
 
 //Store listitems
 const listItems = [];
 
 let dragStartIndex;
-
 
 //Insert list items into DOM
 createList();
@@ -57,15 +59,27 @@ function dragLeave() {
   this.classList.remove('over');
 }
 
-//swapItems first gets the whole DOM element for item being dragged and item being replaced, then swaps the position in the DOM.
+//Swap list items that are drag and drop
 function swapItems(fromIndex, toIndex) {
+  //Grab the whole DOM element for item being dragged and item being replaced
   const itemOne = listItems[fromIndex].querySelector('.draggable');
   const itemTwo = listItems[toIndex].querySelector('.draggable');
 
   listItems[fromIndex].appendChild(itemTwo);
   listItems[toIndex].appendChild(itemOne);
-
 }
+
+//Save the order items were ranked in an array
+function rankedOrder() {
+  const rankedChoices = [];
+  listItems.forEach((listItem) => {
+    const choiceName= listItem.querySelector('.draggable').innerText.trim();
+    rankedChoices.push(choiceName);
+  })
+  console.log(rankedChoices);
+  return rankedChoices;
+}
+
 
 //dragOver exists so that whenever a dragover event happens, the default behaviour is prevented, enabling drop to work.
 function dragOver(e) {
@@ -79,14 +93,11 @@ function dragDrop() {
   swapItems(dragStartIndex, dragEndIndex);
 }
 
-
 //Handle the drag & drop events
 function addEventListeners() {
-
   //Two DOM elements needed: all the draggable classes and draggable-list items, and need to add event listener on each one
   const draggables = document.querySelectorAll('.draggable');
   const dragListItems  = document.querySelectorAll('.draggable-list li');
-
   //listen for dragStart on draggable class (when user starts dragging a choice)
   draggables.forEach( draggable => {
     draggable.addEventListener('dragstart', dragStart);
@@ -101,7 +112,7 @@ function addEventListeners() {
     item.addEventListener('dragenter', dragEnter);
     //dragLeave called when leave an element
     item.addEventListener('dragleave', dragLeave);
-
   })
-
 }
+
+check.addEventListener('click', rankedOrder);
